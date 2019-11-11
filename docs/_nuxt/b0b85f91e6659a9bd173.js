6523d13fa7098038092b75acacec3e65688577e1
asiermusa <asiermusa@gmail.com>
2019-11-11 11:45:30 +0100

    docs folder deleted

--- docs/_nuxt/b0b85f91e6659a9bd173.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[11],{298:function(n,t,e){"use strict";e.r(t);var r={created:function(){this.$router.push("bands")}},c=e(3),component=Object(c.a)(r,function(){var n=this.$createElement;return(this._self._c||n)("div")},[],!1,null,null,null);t.default=component.exports}}]);
--- docs/share.js
+++
@@ -1,3 +0,0 @@
-document.write('<div id="export"></div>')
-let a = document.currentScript.getAttribute('band')
-document.getElementById("export").innerHTML = '<iframe />'
